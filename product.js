ecb96ecec0da2416c6a23fbb6701cd8e644112ca
Clararyehauge <clry0001@stud.kea.dk>
2025-01-30 12:18:44 +0100

    ændringer

--- product.js
+++ product.js
@@ -8,13 +8,14 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
     <h1>Produkt</h1>
         <p>Hjem > Sport > Big Cat Backpack Black</p>
         <div class="grid1_1">
-            <img src="https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp" alt="rygsæk">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="rygsæk">
             <div class="produktinformation">
                 <h1 class="navn_rygsæk">${productdisplayname}</h1>
                 <h2>Mærke</h2>
                 <p class="puma">${data.brandname}</p>
                 <h2>Varenummer</h2>
                 <p class="varenummer">${data.id}</p>
+                <h2>Sæson</h2>
                 <p class="season">${data.season}
                 <a class="indkøbskurv" href="produkt.html">Læg i indkøbskurv</a>
             </div>
