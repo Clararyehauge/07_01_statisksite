8b2729b7619c75867078b394d8e7613848a131ab
Clararyehauge <clry0001@stud.kea.dk>
2025-01-30 13:20:54 +0100

    implementering af fetch

--- produkt.js
+++ produkt.js
@@ -10,14 +10,16 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
         <div class="grid1_1">
             <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="rygsæk">
             <div class="produktinformation">
-                <h1 class="navn_rygsæk">${productdisplayname}</h1>
+                <h1 class="navn_rygsæk">${data.productdisplayname}</h1>
                 <h2>Mærke</h2>
                 <p class="puma">${data.brandname}</p>
+                <h2>Pris</h2>
+                <p class="pris">${data.price}</p>
                 <h2>Varenummer</h2>
                 <p class="varenummer">${data.id}</p>
                 <h2>Sæson</h2>
-                <p class="season">${data.season}
-                <a class="indkøbskurv" href="produkt.html">Læg i indkøbskurv</a>
+                <p class="sæson">${data.season}
+                 <a class="indkøbskurv" href="produkt.html">Læg i indkøbskurv</a>
             </div>
         </div>
 `;
